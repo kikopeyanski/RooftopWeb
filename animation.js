2b23bd35aeb077e96b4e1d222f7cdb74ca525d3c
Xwz <kikopeyanski@gmail.com>
2016-08-01 20:43:10 +0300

    New background images + script fix
    -The content in #homepage-box is now changing every 5 seconds, for
    testing purpose.
    -Some wrapper changes, in order to achieve gray background on main
    content.
    -Absolute position on text and logo.

--- animation.js
+++ animation.js
@@ -1,15 +1,24 @@
- var images = ["Resources/house-roof.jpg", "Resources/roofEx.jpg"];
-    $(function () {
-        var i = 0;
-        $("#toGray").css("background-image", "url(" + images[i] + ")");
-        setInterval(function () {
+var images = ["Resources/house-roof.jpg", "Resources/workplace.jpg"],
+    $main = $("#main"),
+    $secondary = $("#secondary"),
+    sources = [$main, $secondary];
+colors = ["red", "green"]
+
+$(function () {
+    var i = 0;
+    setInterval(function () {
+        sources[i].replaceWith(function () {
+            sources[i].css("display", "none");
             i++;
-            if (i == images.length) {
+            if (i == sources.length) {
                 i = 0;
             }
-            $("#toGray").fadeOut("fast", function () {
-                $(this).css("background-image", "url(" + images[i] + ")");
-                $(this).fadeIn("fast");
-            });
-        }, 1000);
-    });
+            $("#homepage-box").css("background-image", "url(" + images[i] + ")");
+            sources[i].css("display", "block");
+            return sources[i];
+        })
+
+        $(".button").css("background-color", colors[i]);
+        $(".button").css("border-color", colors[i]);
+    }, 5000);
+});
